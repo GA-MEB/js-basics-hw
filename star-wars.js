2f852f3e33e2477502695c9b2b89c88a0c73508a
Matt Brendzel <matt.brendzel@generalassemb.ly>
2016-08-27 20:21:35 -0400

    Put references to LICENSE in code files

--- star-wars.js
+++ star-wars.js
@@ -1,3 +1,6 @@
+// General Assembly, WDI (Web Development Immersive) Remote, Cohort 02 (R2D2)
+// Copyright (C) 2016 Matt Brendzel under the GNU General Public License.
+// See LICENSE for details.
 "use strict";
 
 const net = require('net');
--- test/reach.js
+++ test/reach.js
@@ -1,3 +1,7 @@
+// General Assembly, WDI (Web Development Immersive) Remote, Cohort 02 (R2D2)
+// Copyright (C) 2016 Matt Brendzel under the GNU General Public License.
+// See LICENSE for details.
+
 "use strict";
 
 // 'require' external code //
